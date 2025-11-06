4f2900449128e548a54964c310e6b3b6cfaa763b
44Leonid <hoodrichlenya00@gmail.com>
2025-11-07 01:43:06 +0700

    Delete FORTESTVPNSERVES directory

--- FORTESTVPNSERVES/states.py
+++
@@ -1,9 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-class UserState(StatesGroup):
-    AwaitingAgreement = State()
-    MainMenu = State()
-    AwaitingNewConfig = State()
-    AwaitingBroadcastText = State()
-    AwaitingBroadcastPhoto = State()
-    AwaitingBroadcastUrl = State()
